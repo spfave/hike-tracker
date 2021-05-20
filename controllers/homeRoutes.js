0a83958d9c7b870e1c3ac608e29aacdc972e670c
Sebastian F <sebpfave2@gmail.com>
2021-05-19 20:04:34 -0400

    Create home route path starter functions

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,4 +5,28 @@ router.get('/', async (req, res) => {
   res.send('<h1>Hike Tracker Homepage</h1>');
 });
 
+// Trails
+router.get('/trail/', async (req, res) => {});
+
+// Trail Details
+router.get('/trail/:id', async (req, res) => {});
+
+// User Dashboard
+router.get('/dashboard/', async (req, res) => {});
+
+// User Dashboard - log new hike
+router.get('/dashboard/hike/log', async (req, res) => {});
+
+// User Dashboard - edit saved hike
+router.get('/dashboard/hike/edit/:id', async (req, res) => {});
+
+// User Profile
+router.get('/profile/:id', async (req, res) => {});
+
+// Login
+router.get('/login', (req, res) => {});
+
+// Signup
+router.get('/signup', (req, res) => {});
+
 module.exports = router;
