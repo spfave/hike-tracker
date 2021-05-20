cc43ece65dce7b0881ad149dcd9f0ee8cd3d686b
Sebastian F <sebpfave2@gmail.com>
2021-05-20 18:52:56 -0400

    Update client side view routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,32 +1,29 @@
 const router = require('express').Router();
 
-// Application homepage
+// Application homepage - list of trails and login/signup form
 router.get('/', async (req, res) => {
   res.send('<h1>Hike Tracker Homepage</h1>');
 });
 
-// Trails
-router.get('/trail/', async (req, res) => {});
-
 // Trail Details
 router.get('/trail/:id', async (req, res) => {});
+
+// Trail - create new trail
+router.get('/trail/new/', async (req, res) => {});
+
+// Trail - edit saved trail
+router.get('/trail/edit/:id', async (req, res) => {});
 
 // User Dashboard
 router.get('/dashboard/', async (req, res) => {});
 
 // User Dashboard - log new hike
-router.get('/dashboard/hike/log', async (req, res) => {});
+router.get('/dashboard/hike/new', async (req, res) => {});
 
 // User Dashboard - edit saved hike
 router.get('/dashboard/hike/edit/:id', async (req, res) => {});
 
 // User Profile
-router.get('/profile/:id', async (req, res) => {});
-
-// Login
-router.get('/login', (req, res) => {});
-
-// Signup
-router.get('/signup', (req, res) => {});
+// router.get('/profile/:id', async (req, res) => {});
 
 module.exports = router;
