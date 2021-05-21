17f2ebb7eeb094d0442f7e8ea3bbc3aca75948a7
Sebastian F <sebpfave2@gmail.com>
2021-05-20 22:14:46 -0400

    Create user POST login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,19 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.userId = userData.id;
+      req.session.loggedIn = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
 module.exports = router;
