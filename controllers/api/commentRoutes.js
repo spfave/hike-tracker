24021dd2a1f53b4fb3c38ce2d4e4027ac844128a
Sebastian F <sebpfave2@gmail.com>
2021-05-22 10:34:02 -0400

    Create comment POST route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,4 +1,18 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
+// Create new comment
+router.post('/', async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.userId,
+    });
+
+    res.status(200).json(newComment);
+  } catch (error) {
+    res.status(400).json('Error posting comment, please try again');
+  }
+});
+
 module.exports = router;
