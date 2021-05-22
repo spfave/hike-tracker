c25c8a57d9725e031d99dd1bb09bf51838ade01c
Sebastian F <sebpfave2@gmail.com>
2021-05-22 10:19:37 -0400

    Create user POST login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Sign up new user
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -9,11 +10,50 @@ router.post('/', async (req, res) => {
       req.session.userId = userData.id;
       req.session.loggedIn = true;
 
-      res.status(200).json(userData);
+      res.status(201).json(userData);
     });
   } catch (error) {
-    res.status(400).json(error);
+    res.status(500).json({
+      message: 'Server ran into issue signing you up, please try again',
+    });
   }
 });
 
 module.exports = router;
+
+// Login user
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    const validatePassword = await userData.checkPassword(req.body.password);
+    if (!validatePassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.userId = userData.id;
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: userData, message: 'You are now logged in!' });
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server ran into issue logging you in, please try again',
+    });
+  }
+});
