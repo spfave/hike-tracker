c445b5cdc5418dd9f1b435257ec7aed4bdc295b5
Jung Nam <j.nam0808@gmail.com>
2021-05-25 19:34:13 -0400

    homepage

---
+++ public/js/homepage.js
@@ -0,0 +1,12 @@
+const ranNumbers = [];
+for (let index = 0; index < 5; index++) {
+  //make a random num
+  ranNumbers.push(Math.floot(Math.random() * lengthOfResults));
+}
+console.log(ranNumbers);
+
+fetch('/api/hikeRoutes', {
+  method: 'GET',
+  body: JSON.stringify(ranNumbers),
+  headers: { 'Content-Type': 'application/json' },
+});
