7e08a6a13c4270740fb70e43226bc2be3025203d
Sebastian F <sebpfave2@gmail.com>
2021-05-31 14:10:10 -0400

    Add server file comments and passport setup

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 require('dotenv').config();
+
+// Server and middleware modules/packages
 const express = require('express');
 const session = require('express-session');
+const passport = require('./controllers/passport/passportLocal');
 const path = require('path');
 
+// MVC modules/packages
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/hbs-helper');
 
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/hbs-helper');
+
 const routes = require('./controllers');
 
-const app = express();
+// Server setup
 const PORT = process.env.PORT || 3002;
+const app = express();
 
+// Set public folder path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Session storage
 const sess = {
   secret: process.env.SESS_SECRET,
   cookie: { maxAge: 60 * 60 * 1000 },
@@ -20,17 +30,22 @@ const sess = {
   saveUninitialized: true,
   store: new SequelizeStore({ db: sequelize }),
 };
-const hbs = exphbs.create({ helpers });
-
-// Configure server app
 app.use(session(sess));
 
+// Middleware - passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Configure template view engine
+const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
 app.use(routes);
 
 // Sync database connection and start server
